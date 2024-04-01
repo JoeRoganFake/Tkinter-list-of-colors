0724bb24a4111517df7cf571287ea5247fe59be5
JoeRoganFake <147932206+JoeRoganFake@users.noreply.github.com>
2024-04-01 06:16:24 +0200

    Update main.py
    Function that returns the list

--- main.py
+++ main.py
@@ -1,11 +1,11 @@
 from original_list import original
 
-def Tkinter_list_of_colors(original):
+def Tkinter_list_of_colors(o):
   v1 = ""
   v2=[]
   list_of_colors=[] #final version
   
-  for item in original:
+  for item in o:
       if item!=" ":
           v1+=item
   
@@ -13,12 +13,15 @@ def Tkinter_list_of_colors(original):
         v2.append(item)
   
   for item in v2:
-      original= ""
+      a = ""
       for etem in item:
           if etem !="\n":
-              original+=etem
-      list_of_colors.append(original)
+              a+=etem
+      list_of_colors.append(a)
   return list_of_colors
+
+
+final_list=Tkinter_list_of_colors(original)
 
 
 
